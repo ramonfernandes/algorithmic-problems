f7edce4ac52aaa05cf90f45218ba739a662e1031
Ramon Fernandes <ramonfernandes96@gmail.com>
2018-11-13 20:41:54 -0200

    implementação inicial pronta

--- n-queens-problem/src/Game.java
+++ n-queens-problem/src/Game.java
@@ -5,15 +5,18 @@ public class Game {
 	public Stack<Queen> disposicaoValida(int x, int y, int contador, Stack<Queen> queens) {
 		if (contador == 0)
 			return queens;
-		Stack<Queen> queenCopia = queens;
 		for (int xR = 0; xR < x; xR++) {
 			for (int yR = 0; yR < y; yR++) {
+				Stack<Queen> queenCopia = queens;
 				while (!queenCopia.isEmpty()) {
 					Queen queen = queenCopia.pop();
-					if(queen.equalsXorY(xR, yR)) {
+					if(queen.equalsXorY(xR, yR) || isDiagonal(queen.getX(), queen.getY(), xR, yR)) {
+						queenCopia.push(queen);
 						break;
 					}
 				}
+				if(queenCopia.isEmpty())
+					disposicaoValida(xR, yR, contador-1, addQueenIntoList(queens, new Queen(xR, yR)));
 			}
 		}
 		return null;
@@ -23,6 +26,11 @@ public class Game {
 		if(Math.abs(x - xR) == Math.abs(y - yR))
 			return true;
 		return false;
+	}
+
+	public Stack<Queen> addQueenIntoList(Stack<Queen> stack, Queen queen){
+		stack.add(queen);
+		return stack;
 	}
 
 }
